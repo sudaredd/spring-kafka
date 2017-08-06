d5916592a92cf9cd71629145a37e2c4b34ec0c07
sudaredd <sudaredd@gmail.com>
2017-08-06 16:29:12 -0400

    initial

---
+++ ffc/src/test/java/com/example/ffc/FfcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ffc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FfcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
