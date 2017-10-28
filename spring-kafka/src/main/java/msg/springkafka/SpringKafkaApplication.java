cfc910b23721777be45a63de673fce4435e27ef3
sudaredd <sudaredd@gmail.com>
2017-10-28 09:14:54 -0400

    test to push

--- spring-kafka/src/main/java/msg/springkafka/SpringKafkaApplication.java
+++ spring-kafka/src/main/java/msg/springkafka/SpringKafkaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringKafkaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("kafka ex");
 		SpringApplication.run(SpringKafkaApplication.class, args);
 	}
 }
